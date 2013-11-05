3e0f7025c6ac6aab0da0273d5dd1a9f5df08b4d8
Hina Tufail <htufail@live.fr>
2013-11-05 11:43:07 +0100

    Lien PDF vers signature + CSS

---
+++ application/controllers/SignPdfController.php
@@ -0,0 +1,19 @@
+<?php
+
+class SignPDFController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        
+    	$this->_helper->layout->disableLayout();
+    }
+
+
+}
+
