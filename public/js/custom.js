e48b79aabedfce1ca4c67810f46b45e4cd26ab92
Hina Tufail <htufail@live.fr>
2013-11-11 21:16:59 +0100

    Ajout icônes page affichage document + pop up de Fanny

---
+++ public/js/custom.js
@@ -0,0 +1,33 @@
+jQuery(function($){				   		   
+	//Lorsque vous cliquez sur un lien de la classe poplight
+	$('a.poplight').on('click', function() {
+		var popID = $(this).data('rel'); //Trouver la pop-up correspondante
+		var popWidth = $(this).data('width'); //Trouver la largeur
+		
+		//Faire apparaitre la pop-up et ajouter le bouton de fermeture
+		$('#' + popID).fadeIn().css({ 'width': popWidth}).prepend('<a href="#" class="close"><img class="btn_close" alt="" src="img/icons/close_pop.png"></img></a>');
+		
+		//Récupération du margin, qui permettra de centrer la fenêtre - on ajuste de 80px en conformité avec le CSS
+		var popMargTop = ($('#' + popID).height() + 80) / 2;
+		var popMargLeft = ($('#' + popID).width() + 80) / 2;
+		
+		//Application du margin
+		$('#' + popID).css({ 
+			'margin-top' : -popMargTop,
+			'margin-left' : -popMargLeft
+		});
+		
+		//Apparition du fond - .css({'filter' : 'alpha(opacity=80)'}) pour corriger les bogues d'anciennes versions de IE
+		$('body').append('<div id="popup_fade"></div>');
+		$('#popup_fade').css({'filter' : 'alpha(opacity=80)'}).fadeIn();
+		return false;
+	});
+	
+	//Fermer la popup
+	$('body').on('click', 'a.close, #popup_fade', function() {
+		$('#popup_fade , .popup_block').fadeOut(function() {
+			$('#popup_fade, a.close').remove();  
+	});
+		return false;
+	});
+});
